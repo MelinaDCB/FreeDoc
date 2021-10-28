8ee7db84515343283419b3da824b4ea4892a1802
MelinaDCB <melina.decombas@gmail.com>
2021-10-28 18:29:17 +0200

    final

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_155241) do
+ActiveRecord::Schema.define(version: 2021_10_28_162200) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer "doctor_id"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_10_28_155241) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_appointments_on_city_id"
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
   end
@@ -41,6 +43,8 @@ ActiveRecord::Schema.define(version: 2021_10_28_155241) do
     t.string "zip_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_doctors_on_city_id"
   end
 
   create_table "patients", force: :cascade do |t|
@@ -48,6 +52,8 @@ ActiveRecord::Schema.define(version: 2021_10_28_155241) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_patients_on_city_id"
   end
 
   create_table "specialties", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Appointment.destroy_all
+City.destroy_all
+Doctor.destroy_all
+Patient.destroy_all
+Specialty.destroy_all
+
+clermont = City.create(name: "Clermont-Ferrand")
+paris = City.create(name: "Paris")
+
+doc1 = Doctor.create(first_name: "Meredith", last_name: "Grey", specialty: "general surgery", city: paris)
+doc2 = Doctor.create(first_name: "Alex", last_name: "Karev", specialty: "cardiac surgery", city: clermont)
+
+pat1 = Patient.create(first_name: "Bob", last_name: "Placeholder", city: clermont)
+pat2 = Patient.create(first_name: "John", last_name: "Placeholder", city: paris)
+
+apt1 = Appointment.create(datetime: 2018-09-23T12:30:00+00:00, city: clermont)
+apt2 = Appointment.create(datetime: 2019-05-23T12:30:00+00:00, city: paris)
